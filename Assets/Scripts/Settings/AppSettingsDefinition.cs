790ee9f08737ae52bf9695b9444b27a6be3a96ca
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2024-02-10 15:27:39 +0100

    Draggables cam be thrown

--- Assets/Scripts/Settings/AppSettingsDefinition.cs
+++ Assets/Scripts/Settings/AppSettingsDefinition.cs
@@ -4,13 +4,13 @@ namespace Settings {
     [CreateAssetMenu(menuName = "Settings/AppSettings", fileName = "AppSettings")]
     public class AppSettingsDefinition : ScriptableObject {
         [Header("Scene names")]
-        [Tooltip("Scene to load after loading game")]
+        [Tooltip("Scene to load after loading game.")]
         public string firstSceneName = "MainMenu";
         
-        [Tooltip("Main menu scene")]
+        [Tooltip("Main menu scene.")]
         public string mainMenuSceneName = "MainMenu";
 
-        [Tooltip("Game manager scene name")]
+        [Tooltip("Game manager scene name.")]
         public string gameManagerSceneName = "Game";
         
         [Tooltip("Scene with player's 'house', where mixing and selling takes place.")]
